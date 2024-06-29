2f6058100aba32cb427799729c33aa0cbdd1e40b
Juan_Manuel <Juanmanuelguaquespud@gmail.com>
2024-06-29 12:05:59 -0500

    Segundo punto del taller

---
+++ ejercicio_dos.py
@@ -0,0 +1,37 @@
+# Realice los siguientes programas (uso personalizado)
+
+# Un programa que verifique la igualdad de dos números
+numero_uno_iguales = int(input('Ingrese el primer numero: '))
+numero_dos_iguales  = int(input('Ingrese el segundo numero: '))
+
+print(f'Los numeros son iguales: {numero_uno_iguales  == numero_dos_iguales}')
+
+#  Un programa que verifique si dos números son diferentes
+numero_uno_diferentes = int(input('Ingrese el primer numero: '))
+numero_dos_diferentes = int(input('Ingrese el segundo numero: '))
+
+print(f'Los numeros son diferentes: {numero_uno_diferentes != numero_dos_diferentes}')
+
+#  Un programa que verifique si un numero X es mayor que un numero Y
+numero_uno_mayor = int(input('Ingrese el primer numero: '))
+numero_dos_mayor = int(input('Ingrese el segundo numero: '))
+
+print(f'El numero  uno es mayor: {numero_uno_mayor > numero_dos_mayor}')
+
+# Un programa que verifique si un numero X es menor que un numero Y
+numero_uno_menor = int(input('Ingrese el primer numero: '))
+numero_dos_menor = int(input('Ingrese el segundo numero: '))
+
+print(f'El numero  uno es menor: {numero_uno_menor < numero_dos_menor}')
+
+#Un programa que verifique si un numero X es mayor o igual que un numero Y
+numero_uno_mayor_igual = int(input('Ingrese el primer numero: '))
+numero_dos_mayor_igual = int(input('Ingrese el segundo numero: '))
+
+print(f'El numero  uno es mayor o igual: {numero_uno_mayor_igual >= numero_dos_mayor_igual}')
+
+# Un programa que verifique si un numero X es menor o igual que un numero Y
+numero_uno_menor_igual = int(input('Ingrese el primer numero: '))
+numero_dos_menor_igual = int(input('Ingrese el segundo numero: '))
+
+print(f'El numero  uno es menor o igual: {numero_uno_menor_igual <= numero_dos_menor_igual}')
